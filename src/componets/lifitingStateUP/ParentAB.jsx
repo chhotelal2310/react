b0c9f55c86bb2ed5b167b21d2fe2b176dabd9fdd
Chhotelal Patel <chhotupatel705@gmail.com>
2026-07-10 07:15:51 +0530

    Update

--- src/componets/lifitingStateUP/ParentAB.jsx
+++ src/componets/lifitingStateUP/ParentAB.jsx
@@ -13,3 +13,33 @@ const ParentAB = () => {
 }
 
 export default ParentAB
+
+/********************************* ChildA ********************************/
+import React from "react";
+
+const ChildA = ({ name, setName }) => {
+  return (
+    <div>
+      <p> Inside the ChildA: {name || "--"}</p>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        className="border"
+      />
+    </div>
+  );
+};
+
+export default ChildA;
+
+
+
+
+/********************************* ChildB ********************************/
+import React from "react";
+
+const ChildB = ({ name }) => {
+  return <div>Insdie the childB: {name || "--"}</div>;
+};
+
+export default ChildB;
