22100ef77db2e3ea74c609099bd495c1052cca73
Chhotelal Patel <chhotupatel705@gmail.com>
2026-07-27 08:08:39 +0530

    Update

--- src/componets/hooks/UseStateHook.jsx
+++ src/componets/hooks/UseStateHook.jsx
@@ -1,3 +1,5 @@
+/******************************* Example-1 **********************************/
+
 import React, { useState } from "react";
 
 const UseStateHook = () => {
@@ -18,3 +20,56 @@ const UseStateHook = () => {
 };
 
 export default UseStateHook;
+
+// Flow: User Click → setCount(newValue) → React Updates State → Component Re-renders → UI Updates
+
+/**************************************** Example-3**********************************/
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    console.log("Before:", count);
+
+    setCount(count + 1);
+
+    console.log("After:", count);
+  };
+
+  return (
+    <>
+      <h1>{count}</h1>
+
+      <button onClick={increase}>
+        Increase
+      </button>
+    </>
+  );
+}
+
+// Important Point
+// What do you expect?
+
+// Maybe
+
+// Before: 0
+// After: 1
+
+// But actually you'll get
+// Before: 0
+// After: 0
+
+
+// Explaination:
+// It simply tells React
+
+// "Please update this state."
+
+// React schedules that update.
+
+// After your function finishes,
+
+// React updates the state
+
+// and re-renders the component.
